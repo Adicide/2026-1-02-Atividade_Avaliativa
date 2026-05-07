dd2ac7f5f63533132b6b1620e9f2d04d8e8bab26
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-05-07 10:11:49 -0300

    Delete questao01.py

--- questao01.py
+++
@@ -1,7 +0,0 @@
-# 1. Escreva um programa Python que exiba na tela uma sequência de 10 números inteiros aleatórios entre 1 e 100.
-
-import random
-
-for i in range(10):
-    num = random.randint(1, 100)
-    print(num)
