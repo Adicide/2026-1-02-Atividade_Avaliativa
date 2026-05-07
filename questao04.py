393300d9ce8d08512f74ba34d19c04096914f460
Lucas Gabriel <lucastavaresrego@gmail.com>
2026-05-07 10:12:16 -0300

    Delete questao04.py

--- questao04.py
+++
@@ -1,7 +0,0 @@
-# 4. Escreva um programa que leia um inteiro positivo numero e exibe o número de dígitos de numero.
-
-numero = int(input('Digite um número: '))
-numero_str = str(numero)
-digitos_numero = len(numero_str)
-
-print(f'\nQuantidade de dígitos de {numero}: {digitos_numero}')
